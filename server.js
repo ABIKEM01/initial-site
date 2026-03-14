283af82b521daf4d29fc3b4c86373880b24f052d
Abiodun Babayode <babayod@yahoo.com>
2026-03-14 12:13:52 +0100

    database connection

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ import express from "express"
 import dotenv from "dotenv"
 import path from "path"
 import { fileURLToPath } from "url"
+import { testConnection } from './src/models/db.js';
+import { getAllOrganizations } from './src/models/organizations.js';
 
 dotenv.config()
 
@@ -9,8 +11,8 @@ const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
 
 const app = express()
-const port = process.env.PORT || 3000
-
+const PORT = process.env.PORT || 3000
+const NODE_ENV = process.env.NODE_ENV?.toLowerCase() || 'production';
 // Set EJS as the template engine
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "src", "views"))
@@ -23,9 +25,12 @@ app.get("/", async (req, res) => {
   res.render("home", { title: "Home - Service Network" })
 })
 
-app.get("/organizations", async (req, res) => {
-  res.render("organizations", { title: "Organizations" })
-})
+app.get('/organizations', async (req, res) => {
+    const organizations = await getAllOrganizations();
+    const title = 'Our Partner Organizations';
+
+    res.render('organizations', { title, organizations });
+});
 
 app.get("/projects", async (req, res) => {
   res.render("projects", { title: "Service Projects" })
@@ -35,7 +40,13 @@ app.get("/categories", async (req, res) => {
   res.render("categories", { title: "Project Categories" })
 })
 
-// Start server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`)
-})
+
+app.listen(PORT, async () => {
+  try {
+    await testConnection();
+    console.log(`Server is running at http://127.0.0.1:${PORT}`);
+    console.log(`Environment: ${NODE_ENV}`);
+  } catch (error) {
+    console.error('Error connecting to the database:', error);
+  }
+});
