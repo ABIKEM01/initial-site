42b86fc23a1b4a4dd9bad0694eb364b0ebab47a9
Abiodun Babayode <babayod@yahoo.com>
2026-03-21 13:11:23 +0100

    W03 Learning Activity: Route and Query Parameters

--- src/controllers/routes.js
+++ src/controllers/routes.js
@@ -5,6 +5,7 @@ import { showOrganizationsPage } from './organizations.js';
 import { showProjectsPage } from './projects.js';
 import { showCategoriesPage } from './categories.js';
 import { testErrorPage } from './errors.js';
+import { showOrganizationDetailsPage } from './organizations.js';
 
 const router = express.Router();
 
@@ -12,7 +13,8 @@ router.get('/', showHomePage);
 router.get('/organizations', showOrganizationsPage);
 router.get('/projects', showProjectsPage);
 router.get('/categories', showCategoriesPage);
-
+// Route for organization details page
+router.get('/organization/:id', showOrganizationDetailsPage);
 // error-handling routes
 router.get('/test-error', testErrorPage);
 
